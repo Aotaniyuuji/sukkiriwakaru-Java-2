2b18e5615fd81d697bcc6222257437cf65e57dad
AotaniYuuji <Aotani1998@gmail.com>
2023-09-09 23:17:07 +0900

    Mainクラスで各クラスを元にプログラムを作成

--- スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第8章/Main.java
+++ スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第8章/Main.java
@@ -2,16 +2,25 @@ package スッキリ分かるJava入門第二部第8章;
 
 public class Main {
 	public static void main(String[] args) {
-		//1.勇者インスタンスを生成
+		//勇者生成と初期化
 		Hero h = new Hero();
-		//2.フィールドに初期値をセット
 		h.name = "ミナト";
 		h.hp = 100;
-		System.out.println("勇者" + h.name + "を生み出しました!");
-		//3.勇者のメソッドを呼び出していく
-		h.sit(5); //この場合は5秒座れ
+		
+		//敵A(一匹目)を生成と初期化
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		//敵B(二匹目)を生成と初期化
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+		//冒険の始まり
 		h.slip();
-		h.sit(25);
+		m1.run();
+		m2.run();
 		h.run();
 	}
 }
