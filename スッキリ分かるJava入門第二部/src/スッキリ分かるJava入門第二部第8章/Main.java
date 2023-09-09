74b60853b30b15528c182d73c8bea63bbc793f1a
AotaniYuuji <Aotani1998@gmail.com>
2023-09-09 22:19:58 +0900

    mainクラスにインスタンスを生成

--- スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第8章/Main.java
+++ スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第8章/Main.java
@@ -1,5 +1,12 @@
 package スッキリ分かるJava入門第二部第8章;
 
 public class Main {
-	
+	public static void main(String[] args) {
+		//1.勇者インスタンスを生成
+		Hero h = new Hero();
+		//2.フィールドに初期値をセット
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました!");
+	}
 }
