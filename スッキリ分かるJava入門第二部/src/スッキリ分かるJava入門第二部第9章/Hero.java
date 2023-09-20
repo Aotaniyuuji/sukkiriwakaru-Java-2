2a1c539596e2d479828473d70fe90c1ea4915836
AotaniYuuji <Aotani1998@gmail.com>
2023-09-20 22:53:07 +0900

    has-aの関係

--- スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第9章/Hero.java
+++ スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第9章/Hero.java
@@ -3,4 +3,9 @@ package スッキリ分かるJava入門第二部第9章;
 public class Hero {
 	String name;
 	int hp;
+	Sword sword;
+	public void attack() {
+		System.out.prinntln(this.name + "は攻撃した！");
+		System.out.println("敵に5ポイントのダメージを与えた！");
+	}
 }
---
+++ スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第9章/Sword.java
@@ -0,0 +1,6 @@
+package スッキリ分かるJava入門第二部第9章;
+
+public class Sword {
+	String name;
+	int damage;
+}
