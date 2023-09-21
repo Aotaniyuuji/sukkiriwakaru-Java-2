9b968c46f6972a28442866211c2547989e4f3f78
AotaniYuuji <Aotani1998@gmail.com>
2023-09-21 12:16:22 +0900

    finalでオーバーライドを禁止する

--- スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第10章/Hero.java
+++ スッキリ分かるJava入門第二部/src/スッキリ分かるJava入門第二部第10章/Hero.java
@@ -9,6 +9,10 @@ public class Hero {
 		m.hp -= 5;
 		System.out.println("5ポイントのダメージを与えた!");
 	}
+	public final void slip() { //finalがついているのでオーバーライドできない
+		System.out.println(this.name + "は転んだ!");
+		System.out.println("5のダメージ");
+	}
 	//逃げる
 	public void run() {
 		System.out.println(this.name + "は逃げ出した!");
